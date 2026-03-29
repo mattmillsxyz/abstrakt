40aa27136649c932597aef5a3ef4bacc14c6ffc3
Matt Mills <mattmillsxyz@gmail.com>
2026-03-29 15:33:33 -0500

    Initial Commit

---
+++ abstrakt/abstraktApp.swift
@@ -0,0 +1,17 @@
+//
+//  abstraktApp.swift
+//  abstrakt
+//
+//  Created by Matt Mills on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct abstraktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ abstraktTests/abstraktTests.swift
@@ -0,0 +1,17 @@
+//
+//  abstraktTests.swift
+//  abstraktTests
+//
+//  Created by Matt Mills on 3/29/26.
+//
+
+import Testing
+@testable import abstrakt
+
+struct abstraktTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
